d39439c88284c1032a8c51d77c5da40299264b7c
erickolk <erickdynsouza@hotmail.com>
2023-04-13 15:22:52 -0300

    Atualização

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
-const data = [
-    { label: 'Janeiro', value: 500 },
-    { label: 'Fevereiro', value: 750 },
-    { label: 'Março', value: 1000 },
-    { label: 'Abril', value: 650 },
-    { label: 'Maio', value: 800 },
-];
+// const data = [
+//     { label: 'Janeiro', value: 500 },
+//     { label: 'Fevereiro', value: 750 },
+//     { label: 'Março', value: 1000 },
+//     { label: 'Abril', value: 650 },
+//     { label: 'Maio', value: 800 },
+// ];
 
-window.addEventListener('DOMContentLoaded', () => {
-    const cards = document.querySelectorAll('.card');
+// window.addEventListener('DOMContentLoaded', () => {
+//     const cards = document.querySelectorAll('.card');
 
-    cards.forEach(card => {
-        const chart = document.createElement('div');
-        chart.classList.add('chart');
+//     cards.forEach(card => {
+//         const chart = document.createElement('div');
+//         chart.classList.add('chart');
 
-        for (let i = 0; i < data.length; i++) {
-            const barContainer = document.createElement('div');
-            barContainer.classList.add('bar-container');
+//         for (let i = 0; i < data.length; i++) {
+//             const barContainer = document.createElement('div');
+//             barContainer.classList.add('bar-container');
 
-            const bar = document.createElement('div');
-            bar.classList.add('bar');
-            bar.style.height = `${data[i].value / 10}px`;
-            bar.setAttribute('data-label', data[i].label);
+//             const bar = document.createElement('div');
+//             bar.classList.add('bar');
+//             bar.style.height = `${data[i].value / 10}px`;
+//             bar.setAttribute('data-label', data[i].label);
 
-            barContainer.appendChild(bar);
-            chart.appendChild(barContainer);
-        }
+//             barContainer.appendChild(bar);
+//             chart.appendChild(barContainer);
+//         }
 
-        card.appendChild(chart);
-    });
-});
+//         card.appendChild(chart);
+//     });
+// });
